d65300da5d08d5c9ec22460b1e4b1278084c24aa
Alex <alexanderlinus@gmail.com>
2023-09-13 23:17:58 -0700

    added stuff 5

--- Encounter.java
+++ Encounter.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Encounter {
+
+
+    public static void email()//email
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("First, let's cast our eyes upon our electronic treasure chest and see if we've plundered any digital messages.");
+        Main.wait(5);
+        System.out.println("Ahoy, it seems there be a fresh scroll in the inbox!");
+        Main.wait(3);
+        System.out.println("Enter yer secret passphrase to unfurl the message, matey:");
+        String input = scan.next();
+        Main.wait(1);
+        System.out.println("[Hey what's up just letting you know that there's some new stuff on pirate bay." +
+                "\nIt's pretty cool. you should check it out." +
+                "\nps if you get the chance could you send me the textbook for the class? ty.]");
+        Main.wait(10);
+        System.out.println("\nHmm, it be lookin' like we ought to make a course for Pirate Bay." +
+                "\nBut mayhaps we should pay a visit to the library if fortune smiles upon us.");
+    }
 }
--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args)
     {
         intro();
-        playerResponse();
+        introPlayerResponse();
         System.out.println("test");
         clearScreen();
-        encounter1();
+        Encounter.email();
+
 
     }
     public static void intro()
@@ -27,7 +28,7 @@ public class Main {
             Thread.currentThread().interrupt();
         }
     }
-    public static void playerResponse()
+    public static void introPlayerResponse()
     {
         Scanner scan = new Scanner(System.in);
         String response = scan.nextLine();
@@ -50,39 +51,31 @@ public class Main {
     public static void clearScreen() {
         for (int i = 0; i < 50; ++i) System.out.println();
     }
-    public static void encounter1()//email
+
+    public static void askNext(int encounter)
     {
         Scanner scan = new Scanner(System.in);
-        System.out.println("First, let's cast our eyes upon our electronic treasure chest and see if we've plundered any digital messages.");
-        wait(5);
-        System.out.println("Ahoy, it seems there be a fresh scroll in the inbox!");
-        wait(1);
-        System.out.println("Enter yer secret passphrase to unfurl the message, matey:");
-        String input = scan.next();
-        wait(1);
-        System.out.println("[Hey what's up just letting you know that there's some new stuff on pirate bay." +
-                "\nIt's pretty cool. you should check it out." +
-                "\nps if you get the chance could you stop by the library for me? ty.]");
-        wait(10);
-        System.out.println("\nHmm, it be lookin' like we ought to make a course for Pirate Bay." +
-                "\nBut mayhaps we should pay a visit to the library if fortune smiles upon us.");
+        switch(encounter)
+        {
+            case 1:
+                System.out.println("encounter 1");
+                break;
+            case 2:
+                System.out.println("encounter 2");
+                break;
+            case 3:
+                System.out.println("encounter 3");
+                break;
+            case 4:
+                System.out.println("encounter 4");
+                break;
+            case 5:
+                System.out.println("encounter 5");
+                break;
+        }
     }
-    public static void encounter2()
-    {
-        System.out.println("placeholder text 2");
-    }
-    public static void encounter3()
-    {
-        System.out.println("placeholder text 3");
-    }
-    public static void encounter4()
-    {
-        System.out.println("placeholder text 4");
-    }
-    public static void encounter5()
-    {
-        System.out.println("placeholder text 5");
-    }
+
+
 
 
 
