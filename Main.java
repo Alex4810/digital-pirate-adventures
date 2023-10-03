90a74b68b797c5422f06e794257ed39cf560e46c
Alex <alexanderlinus@gmail.com>
2023-10-03 15:30:17 -0700

    working on fixing game 2

--- Main.java
+++ Main.java
@@ -45,10 +45,11 @@ public class Main {
         }
 
         public static void intro() {
-            System.out.println("Ahoy there fellow digital pirate!\n"
-                    +"Welcome aboard yer trust ship and prepare to sail the internet seas!\n"
-                    +"I be yer trusty virtual quartermaster.\n"
-                    +"Ready to embark on this digital adventure? (y/n)");
+            System.out.println("""
+                    Ahoy there fellow digital pirate!
+                    Welcome aboard yer trust ship and prepare to sail the internet seas!
+                    I be yer trusty virtual quartermaster.
+                    Ready to embark on this digital adventure? (y/n)""");
         }
         public static void introPlayerResponse() {
             Scanner scan = new Scanner(System.in);
@@ -67,32 +68,53 @@ public class Main {
         }
         public static void setup()
         {
-            System.out.println("Ahoy! Before we set sail, what be yer name, matey?");
-            Ship.playerName = scan.nextLine();
-            System.out.println("And what be the name of our ship?");
-            Ship.shipName = scan.nextLine();
+            askPlayerName();
             System.out.println("No finer names for the high seas, indeed!");
         }
 
+        public static void askPlayerName()
+        {
+            System.out.println("Ahoy! Before we set sail, what be yer name, matey?");
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                System.out.println("Looks like ye didn't input anything or just spaces, matey. What be yer name?");
+                askPlayerName();
+            }
+            else    {
+                Ship.playerName = input;
+            }
+        }
+        public static void askShipName()
+        {
+            System.out.println("And what be the name of our ship?");
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                System.out.println("Looks like ye didn't input anything or just spaces, matey. What ye be callin' yer ship?");
+                askPlayerName();
+            }
+            else    {
+                Ship.shipName = input;
+            }
+        }
+
+
         public static Ship askShipType()
         {
-            System.out.println("What be the type o' yer ship, matey? (1/2/3)\n"
-                              +"1. Sloop - fastest speed, lowest firepower\n"
-                              +"2. Frigate - balanced speed n' firepower\n"
-                              +"3. Galleon - superior firepower, slowest speed");
+            System.out.println("""
+                    What be the type o' yer ship, matey? (1/2/3)
+                    1. Sloop - fastest speed, lowest firepower
+                    2. Frigate - balanced speed n' firepower
+                    3. Galleon - superior firepower, slowest speed""");
             String input = scan.nextLine();
-            switch(input)
-            {
-                case "1":
-                    return new Sloop(200, 100, 10, 7, 0.1, 0, 0.75);
-                case "2":
-                    return new Frigate(350,250,7,3,0.20,3,100);
-                case "3":
-                    return new Galleon(500,200,4,1,0.30,6,1.25);
-                default:
+            return switch (input) {
+                case "1" -> new Sloop(200, 100, 10, 7, 0.1, 0, 0.75);
+                case "2" -> new Frigate(350, 250, 7, 3, 0.20, 3, 100);
+                case "3" -> new Galleon(500, 200, 4, 1, 0.30, 6, 1.25);
+                default -> {
                     System.out.println("Arr, I didn't quite catch that. Input 1/2/3, to choose yer ship type, matey.");
-                    return askShipType();
-            }
+                    yield askShipType();
+                }
+            };
 
         }
         public static void embark()
@@ -134,14 +156,6 @@ public class Main {
 
         public static int d20() {
             return roll.nextInt(20) + 1;
-        }
-        public static void wait(int secs) {
-            int milis = secs * 1000;
-            try {
-                Thread.sleep(milis);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
         }
 
 
--- Ship.java
+++ Ship.java
@@ -5,18 +5,18 @@ public class Ship {
     public static String playerName;
     public static String shipName;
 
-    public int health =0;
-    public int bandwidth = 0;
-    public int speed = 0;
-    public int dodge = 0;
-    public double armor = 0;
-    public int accuracy = 0;
-    public double damage = 0;
-    public int broadsideType = 0;
-    public int swivelType = 0;
-    public int crewType = 0;
-    public int sailType = 0;
-    public int riggingType = 0;
+    public int health;
+    public int bandwidth;
+    public int speed;
+    public int dodge;
+    public double armor;
+    public int accuracy;
+    public double damage;
+    public int broadsideType;
+    public int swivelType;
+    public int crewType;
+    public int sailType;
+    public int riggingType;
     public boolean swivel = true;
 
 
@@ -33,9 +33,11 @@ public class Ship {
     public void setBroadside(int broadsideType)
     {
         System.out.println("Yer broadside cannons be yer main attack. What type of cannon be yer choice? (1/2/3)\n");
-        System.out.println("1. DDOS Cannon - Devastatin' damage, but accuracy be lackin.\n" +
-                "2. DNS Leak Launchers - Medium damage n' accuracy; a safe choice\n" +
-                "3. Zero Day Exploit Guns - They be strikin' true with high accuracy, but lower damage.\n");
+        System.out.println("""
+                1. DDOS Cannon - Devastatin' damage, but accuracy be lackin.
+                2. DNS Leak Launchers - Medium damage n' accuracy; a safe choice
+                3. Zero Day Exploit Guns - They be strikin' true with high accuracy, but lower damage.
+                """);
         String input = scan.nextLine();
         switch(input)
         {
@@ -60,9 +62,11 @@ public class Ship {
     public void setSwivel(int swivelType)
     {
         System.out.println("Yer swivel guns be a choice secondary gun. Smaller, but more precise. What be yer choice o' swivel? (1/2/3)\n");
-        System.out.println("1. Zip Bomb Swivels - Load yer swivel guns with zip bombs to confuse yer enemy and lower their accuracy./n\n" +
-                            "2. IP-Trace Swivels - Load em' with shots that trace yer enemies' digital footprints, boostin' yer accuracy.\n" +
-                            "3. Logic Bomb Swivels - Throw yer caution to the sea, unleashing pure RNG chaos and causin' unpredictable damage with every shot!\n");
+        System.out.println("""
+                1. Zip Bomb Swivels - Load yer swivel guns with zip bombs to confuse yer enemy and lower their accuracy./n
+                2. IP-Trace Swivels - Load em' with shots that trace yer enemies' digital footprints, boostin' yer accuracy.
+                3. Logic Bomb Swivels - Throw yer caution to the sea, unleashing pure RNG chaos and causin' unpredictable damage with every shot!
+                """);
         String input = scan.nextLine();
         switch(input)
         {
@@ -87,8 +91,10 @@ public class Ship {
     public void setCrew(int crewType)
     {
         System.out.println("Our ship be havin' room for extra crew abilities. What be yer choice for em', matey? (1/2)\n");
-        System.out.println("1. Latency Musket Volley - Rally yer crew to fire a musket volley at yer enemy, dealin' damage and disruptin' their systems.\n" +
-                            "2. Overclock Powder Kegs - Push yer crew to the limit, increasin' yer damage while sacrificin' some o' yer armor.\n");
+        System.out.println("""
+                1. Latency Musket Volley - Rally yer crew to fire a musket volley at yer enemy, dealin' damage and disruptin' their systems.
+                2. Overclock Powder Kegs - Push yer crew to the limit, increasin' yer damage while sacrificin' some o' yer armor.
+                """);
         String input = scan.nextLine();
         switch(input)
         {
@@ -109,8 +115,10 @@ public class Ship {
     public void setSails(int sailType, int dodge, int speed)
     {
         System.out.println("Our ship's sails be the lifeblood o' our vessel. What kind o' sails be on yer ship? (1/2)\n");
-        System.out.println("1. VPN Sails - Gives our ship extra maneuverability while sacrificin' some speed, navigatin' the waves with stealth.\n" +
-                "2. Fiber Optic Sails - Harness the speed of fiber optics, lettin' yer ship race through the waters\n");
+        System.out.println("""
+                1. VPN Sails - Gives our ship extra maneuverability while sacrificin' some speed, navigatin' the waves with stealth.
+                2. Fiber Optic Sails - Harness the speed of fiber optics, lettin' yer ship race through the waters
+                """);
         String input = scan.nextLine();
         switch(input)
         {
@@ -135,8 +143,10 @@ public class Ship {
     public void setRigging(int riggingType, double armor, int speed)
     {
         System.out.println("Our ship not be complete without proper rigging. What kind o' rigging be on yer ship? (1/2)\n");
-        System.out.println("1. Anti-virus Rigging - Fortify yer defenses, makin yer ship tougher while sacrificin' some speed\n" +
-                "2. Tune-up Utility Rigging - Keep yer ship in top condition, allowing it to perform at its maximum speed.\n");
+        System.out.println("""
+                1. Anti-virus Rigging - Fortify yer defenses, makin yer ship tougher while sacrificin' some speed
+                2. Tune-up Utility Rigging - Keep yer ship in top condition, allowing it to perform at its maximum speed.
+                """);
         String input = scan.nextLine();
         switch(input)
         {
